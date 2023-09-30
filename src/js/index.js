e314127a733ecd823d75e0126fc562b4ec5f9ac6
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2023-09-30 18:26:42 +0300

    changes-3

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,17 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
 
 const breedSelect = document.querySelector('.breed-select');
-
 const infoLoad = document.querySelector('.loader');
 const Error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
 window.addEventListener('load', init);
-// start
+
 function init() {
     let breedsData;
+
+    infoLoad.classList.remove('hidden');
+
     fetchBreeds()
         .then(data => {
             breedsData = data;
@@ -19,21 +21,21 @@ function init() {
                 option.value = breed.id;
                 option.textContent = breed.name;
                 breedSelect.appendChild(option);
-
                 breedSelect.classList.remove('hidden');
                 infoLoad.classList.add('hidden');
-            })
+            });
         })
-  // catch
         .catch(error => {
             console.log(error);
+
             Error.classList.remove('hidden');
             breedSelect.classList.add('hidden');
             infoLoad.classList.add('hidden');
-        })
-// breed
+        });
+
     breedSelect.addEventListener('change', () => {
         const selectBreedId = breedSelect.value;
+
         infoLoad.classList.remove('hidden');
         catInfo.classList.add('hidden');
         Error.classList.add('hidden');
@@ -51,17 +53,16 @@ function init() {
             })
             .catch(error => {
                 console.log(error);
+
                 Error.classList.remove('hidden');
                 infoLoad.classList.add('hidden');
-            })
+            });
     });
-};
+}
 
-
-// Mark
 function createMarkup(catData, breedData) {
     return `<img src='${catData.url}' width='400' alt='${breedData.name}'/>
         <div class='textInfo'><h1>${breedData.name}</h1>
         <p>${breedData.description}</p>
-        <p><b>Temperament:</b> ${breedData.temperament}</p></div>`
-};
+        <p><b>Temperament:</b> ${breedData.temperament}</p></div>`;
+}
